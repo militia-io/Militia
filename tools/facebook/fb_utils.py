f3bd4ddd8df90a09c1e8da6663be6a08529ea3e2
V Lall <v@trendhigh.com>
2015-06-20 00:03:15 -0400

    militia tools

---
+++ tools/facebook/fb_utils.py
@@ -0,0 +1,36 @@
+import json
+import mechanize
+
+class Load_FB:
+	#Facebook Mechanize session
+	def __init__(self):
+		with open('../config.json', 'r') as file:
+			config = json.load(file)
+		self._email = config['email']
+		self._pass = config['pass']
+		# Set Location of data file
+		self._input = '../' + config['input_file']
+		self._output = config['output_file']
+		self._api = config['gmaps-api-key']
+
+ 		browser = mechanize.Browser()
+		browser.set_handle_robots(False)
+		cookies = mechanize.CookieJar()
+		browser.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US)     AppleWebKit/534.7 (KHTML, like Gecko) Chrome/7.0.517.41 Safari/534.7')]
+		browser.open("https://www.facebook.com/login.php")
+		browser.select_form(nr=0)
+
+		# Enter Facebook information of user that's performing the search
+		browser.form['email'] = self._email
+		browser.form['pass'] = self._pass
+		response = browser.submit()
+		self.browser = browser
+		self.response = response
+
+	def read_Response(self):
+		 return self.response.read()
+		
+if __name__ == '__main__': 
+	#Debugging
+	Load_FB()
+	print Load_FB().read_Response()
---
+++ tools/google/gmap_utils.py
@@ -0,0 +1,20 @@
+import json
+
+class Gmap:
+	def __init__(self):
+		with open('../config.json', 'r') as file:
+			config = json.load(file)
+		_api = config['gmaps-api-key']
+
+	def get_GPS(self, city, key):
+		city = city.replace(' ', '')
+		#try:
+		url ='https://maps.googleapis.com/maps/api/geocode/json?address=%s&sensor=false&key=%s' % (city, key)
+		response = urllib2.urlopen(url)
+		geoCode = json.load(response)
+		#latitude = geoCode['results']['geometry']['bounds']['northeast']['lat'] 
+		#longitude = geoCode['results']['geometry']['bounds']['northeast']['lng'] 
+		#pprint(geoCode)
+		lat = geoCode['results'][0]['geometry']['bounds']['northeast']['lat']
+		lng = geoCode['results'][0]['geometry']['bounds']['northeast']['lng']
+		return lat,lng
